c2c68c7a4e821661abb8c6e72d91f59a406464c6
Volodymyr Samodid <vladimir.samoded@onat.edu.ua>
2014-09-24 09:15:07 +0300

    video lesson 5 and 6. Done.

--- hackernews/hackernews/urls.py
+++ hackernews/hackernews/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hackernews.views.home', name='home'),
--- hackernews/stories/admin.py
+++ hackernews/stories/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from models import Story
 # Register your models here.
+
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'domain', 'moderator', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title','moderator__username')
+
+    fieldsets = [
+        ('Story', {
+            'fields': ('title', 'url', 'points')
+        }),
+        ('Moderator', {
+            'classes': ('collapse',),
+            'fields': ('moderator',)
+        }),
+        ('Change History', {
+            'classes': ('collapse',),
+            'fields': ('created_at', 'updated_at')
+        })
+    ]
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Story, StoryAdmin)
