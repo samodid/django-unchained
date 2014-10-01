8bb473f91f644147dd70a65aa5bb33cdb284531d
Volodymyr Samodid <vladimir.samoded@onat.edu.ua>
2014-10-01 22:35:20 +0300

    lesson 8. Done

--- hackernews/stories/views.py
+++ hackernews/stories/views.py
@@ -1,8 +1,7 @@
 import datetime
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from django.utils.timezone import utc
-from future_builtins import  ascii
+
 
 from future_builtins import  ascii
 from .models import Story
@@ -17,22 +16,9 @@ def score(story, gravity=1.8, timebase=120):
 def top_stories(top=180, consider=1000):
     latest_stoties = Story.objects.all().order_by('-created_at')[:consider]
     ranked_stories = sorted([(score(story), story) for story in latest_stoties], reverse=True)
-    return [story for _, story in ranked_stories[:top]]
+    return [story for _, story in ranked_stories[: top]]
 
 def index(request):
     stories = top_stories(top=30)
-    response = '''
-    <!DOCTYPE html>
-    <html>
-        <head lang="en">
-        <meta charset="UTF-8">
-        <title>Tuts+ News</title>
-        </head>
-    <body>
-        <ol>
-        {}
-        </ol>
-    </body>
-    </html>
-    '''.format('\n'.join(['<li>{}</li>'.format(ascii(story.title)) for story in stories]))
-    return HttpResponse(response)
+
+    return render_to_response('stories/index.html', {'stories': stories})
